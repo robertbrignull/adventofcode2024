9b38db3e0272bd3f31a31f049f41aaa00b7603e0
Robert <robertbrignull@github.com>
2024-12-10 17:47:59 +0000

    Implement day 5 part 2

--- app/models/days/day5.rb
+++ app/models/days/day5.rb
@@ -9,6 +9,11 @@ module Days
     end
 
     def part2
+      invalid_updates = updates.filter do |ps|
+        !rules.all? { |r| r.test(ps) }
+      end
+
+      invalid_updates.map { |ps| get_middle_page(fix_pages(ps)) }.sum
     end
 
     private
@@ -38,6 +43,28 @@ module Days
     def get_middle_page(pages)
       raise "No middle page" if pages.length % 2 == 0
       pages[(pages.length - 1) / 2]
+    end
+
+    def fix_pages(pages)
+      loop do
+        pages, changed = apply_all_rules(pages)
+        break unless changed
+      end
+      pages
+    end
+
+    def apply_all_rules(pages)
+      changed = false
+      new_pages = pages.dup
+      rules.each do |r|
+        a = new_pages.find_index(r.a)
+        b = new_pages.find_index(r.b)
+        if a && b && a > b
+          new_pages[a], new_pages[b] = new_pages[b], new_pages[a]
+          changed = true
+        end
+      end
+      [ new_pages, changed ]
     end
   end
 end
